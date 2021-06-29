020e68229c99775bc190779fceba8fcb1e5e4726
valerian alphonso <valerian.alphonso@flexiloans.com>
2021-06-29 11:17:20 +0530

    Block Scope

---
+++ Blockscope/block.js
@@ -0,0 +1,37 @@
+var i=10;
+let j=20;
+const k=30;
+console.log(i);
+console.log(j);
+console.log(k);
+console.log("------");
+
+{
+    var i=50;
+    let j=60;
+    const k=70; 
+    
+    console.log(i);
+    console.log(j);
+    console.log(k);
+    console.log("------");
+}
+
+console.log(i);
+console.log(j);
+console.log(k);
+console.log("-------------");
+
+var a=1;
+for (var a=10;a<15;a++){
+    console.log(a);
+}
+console.log(a);
+console.log("-----");
+
+let b=2;
+for (let b=10;b<15;b++){
+    console.log(b);
+}
+console.log(b);
+
